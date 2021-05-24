dff3d63788c744923bd476e24f0da145c18eb070
mukaseevru <github@mukaseev.ru>
2021-05-24 21:58:27 +0300

    update describe.py

--- describe.py
+++ describe.py
@@ -1,6 +1,6 @@
 import argparse
 import pandas as pd
-import numpy as np
+# import numpy as np
 import math
 import os
 pd.options.display.float_format = '{:.5f}'.format
@@ -14,65 +14,53 @@ def parse_args():
 
 
 def describe_count(lst):
-    count = 0
-    for elem in lst:
-        if pd.isna(elem):
-            continue
-        count += 1
-    return count
+    lst = [x for x in lst if not pd.isna(x)]
+    return len(lst)
 
 
 def describe_mean(lst):
-    len_lst = len(lst)
-    sum_lst = 0.0
-    for elem in lst:
-        if pd.isna(elem):
-            len_lst -= 1
-            continue
-        sum_lst += elem
-    return sum_lst / len_lst
+    lst = [x for x in lst if not pd.isna(x)]
+    return sum(lst) / len(lst)
 
 
 def describe_std(lst):
+    lst = [x for x in lst if not pd.isna(x)]
     sum_lst = 0.0
     count_lst = describe_count(lst) - 1
     mean_lst = describe_mean(lst)
     for elem in lst:
-        if pd.isna(elem):
-            continue
         sum_lst += (elem - mean_lst) ** 2
     return (sum_lst / count_lst) ** 0.5
 
 
 def describe_min(lst):
+    lst = [x for x in lst if not pd.isna(x)]
     min_lst = lst[0]
     for elem in lst:
-        if pd.isna(elem):
-            continue
-        if elem < min_lst or pd.isna(min_lst):
+        if elem < min_lst:
             min_lst = elem
     return min_lst
 
 
 def describe_max(lst):
+    lst = [x for x in lst if not pd.isna(x)]
     max_lst = lst[0]
     for elem in lst:
-        if pd.isna(elem):
-            continue
-        if elem > max_lst or pd.isna(max_lst):
+        if elem > max_lst:
             max_lst = elem
     return max_lst
 
 
 def describe_percent(lst, percent):
+    lst = [x for x in lst if not pd.isna(x)]
     lst = sorted(lst)
     k = (len(lst) - 1) * percent
     f = math.floor(k)
     c = math.ceil(k)
     if f == c:
         return lst[int(k)]
-    d0 = lst[int(f)] * (c-k)
-    d1 = lst[int(c)] * (k-f)
+    d0 = lst[int(f)] * (c - k)
+    d1 = lst[int(c)] * (k - f)
     return d0 + d1
 
 
@@ -98,13 +86,13 @@ def main():
     if os.path.exists(args['path']):
         try:
             df = pd.read_csv(args['path'])
+            print(describe(df))
         except OSError as e:
             print('Cannot open file:', e)
         except Exception as e:
             print('Unknown error:', e)
     else:
         exit('False path to data.csv. Try: \'python3 describe.py --path data/dataset_train.csv\'')
-    print(describe(df))
 
 
 if __name__ == '__main__':
