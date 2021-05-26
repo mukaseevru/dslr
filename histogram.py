a783f287045b2a2e2edf3cc488d40fe04c0777ea
mukaseevru <github@mukaseev.ru>
2021-05-26 22:27:17 +0300

    update histogram.py

--- histogram.py
+++ histogram.py
@@ -11,34 +11,38 @@ def parse_args():
     return args.__dict__
 
 
+def histogram(df):
+    features = df.select_dtypes(include=['int64', 'float64']).drop('Index', axis=1).columns
+    houses = ['Slytherin', 'Gryffindor', 'Ravenclaw', 'Hufflepuff']
+    houses_dct = {}
+    for house in houses:
+        temp = df[df['Hogwarts House'] == house]
+        houses_dct[house] = temp.select_dtypes(include=['int64', 'float64']).dropna()
+    fig, axes = plt.subplots(4, 4, figsize=(15, 10))
+    fig.suptitle('Histograms')
+    for i, feature in enumerate(features):
+        plt.subplot(5, 3, i + 1)
+        plt.hist(houses_dct['Slytherin'][feature], 50, density=True,
+                 color='red', alpha=0.75, label='Slytherin')
+        plt.hist(houses_dct['Gryffindor'][feature], 50, density=True,
+                 color='blue', alpha=0.75, label='Gryffindor')
+        plt.hist(houses_dct['Ravenclaw'][feature], 50, density=True,
+                 color='green', alpha=0.75, label='Ravenclaw')
+        plt.hist(houses_dct['Hufflepuff'][feature], 50, density=True,
+                 color='orange', alpha=0.75, label='Hufflepuff')
+        plt.title(feature)
+        plt.legend()
+    fig.tight_layout()
+    plt.savefig('plots/histogram.png')
+    print('You can see histograms here: plots/histogram.png')
+
+
 def main():
     args = parse_args()
     if os.path.exists(args['path']):
         try:
             df = pd.read_csv(args['path'])
-            features = df.select_dtypes(include=['int64', 'float64']).drop('Index', axis=1).columns
-            houses = ['Slytherin', 'Gryffindor', 'Ravenclaw', 'Hufflepuff']
-            houses_dct = {}
-            for house in houses:
-                temp = df[df['Hogwarts House'] == house]
-                houses_dct[house] = temp.select_dtypes(include=['int64', 'float64']).dropna()
-            fig, axes = plt.subplots(4, 4, figsize=(15, 10))
-            fig.suptitle('Histograms')
-            for i, feature in enumerate(features):
-                plt.subplot(5, 3, i+1)
-                plt.hist(houses_dct['Slytherin'][feature], 50, density=True,
-                         color='red', alpha=0.75, label='Slytherin')
-                plt.hist(houses_dct['Gryffindor'][feature], 50, density=True,
-                         color='blue', alpha=0.75, label='Gryffindor')
-                plt.hist(houses_dct['Ravenclaw'][feature], 50, density=True,
-                         color='green', alpha=0.75, label='Ravenclaw')
-                plt.hist(houses_dct['Hufflepuff'][feature], 50, density=True,
-                         color='orange', alpha=0.75, label='Hufflepuff')
-                plt.title(feature)
-                plt.legend()
-            fig.tight_layout()
-            plt.savefig('plots/histogram.png')
-            print('You can see histograms here: plots/histogram.png')
+            histogram(df)
         except OSError as e:
             print('Cannot open file:', e)
         except Exception as e:
